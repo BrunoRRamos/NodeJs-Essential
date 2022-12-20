7151a07d10bc234c3c69c1d3ae984a1c65b280bc
MisterBeamCs <103748132+BrunoRRamos@users.noreply.github.com>
2022-12-20 17:41:35 -0300

    fix
    error fix

--- SearchBrokenLinks/src/cli.js
+++ SearchBrokenLinks/src/cli.js
@@ -4,23 +4,45 @@ import catchArchiveAsync from './index.js';
 
 const way = process.argv;
 
+function printList(result) {
+    console.log(chalk.yellow("Link List:"), result)
+}
+
 function noLinks(error) {
     throw new Error(chalk.red(error.code, "Não há links."))
 }
 
+function noSutchFile(error) {
+   throw new Error(chalk.red(error.code, "- No sutch archive or dir."))
+}
+
+function verifyWay(way) {
+    try {
+        fs.statSync(way);
+    } catch (error) {
+        noSutchFile(error);
+    }
+}
+
 async function logLinkInfo(args) {
+    verifyWay(args);
     const infoLinks = await catchArchiveAsync(args)
+
         if (infoLinks.length === 0) {
             console.log(chalk.yellow("Link list"), chalk.red("No links"));
+
         } else {
             console.log(chalk.yellow("Link list"));
             infoLinks
-            .forEach(Element => {console.log(chalk.green(JSON.stringify(Element)))})
+            .forEach(Element => {
+                console.log(chalk.green(JSON.stringify(Element)))
+            })
         }
 }
 
 async function textProcess(args) {
-    const way = args[2]
+    const way = args[2];
+    verifyWay(way);
 
     if (fs.lstatSync(way).isFile()) {
         return await logLinkInfo(way);
@@ -29,8 +51,11 @@ async function textProcess(args) {
     else if (fs.lstatSync(args).isDirectory()) {
         const archives = fs.promises.readdir(way)
         
+        archives.array.forEach(async (archiveName) => {
+            const list = await catchArchiveAsync(`${way}/${archiveName}`)
+            printList(list)
+        });
     }
-
- }
+}
 
 textProcess(way)
