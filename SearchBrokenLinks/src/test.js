94cd742c5de160866a7727f53c7f2e4b9cc81193
MisterBeamCs <103748132+BrunoRRamos@users.noreply.github.com>
2022-12-21 01:34:24 -0300

    finalizado

--- SearchBrokenLinks/src/test.js
+++
@@ -1,9 +0,0 @@
-const links = ["https://gatinhosalsicha.com.br/"]
-
-async function test(links) {
-    links.forEach(async (element) => {
-        const res = await fetch(element)
-        console.log(res.status)
-    });
-}
-test(links)
